7e0fedc77a1cc4fbd60c966f79bf183163220a8f
ADITYA PANDEY <140912316+adityapandey2002@users.noreply.github.com>
2025-06-15 20:35:58 +0530

    handling post signup functionality

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -9,7 +9,7 @@ const authRouter = express.Router();
 authRouter.get("/login", authController.getAuthPage);
 authRouter.post("/login", authController.postLogin);
 authRouter.post("/logout", authController.postLogout);
-authRouter.get("/SignUp", authController.getSignUpPage);
-authRouter.post("/SignUp", authController.postSignUp);
+authRouter.get("/signup", authController.getSignUpPage);
+authRouter.post("/signup", authController.postSignUp);
 
 exports.authRouter = authRouter;
